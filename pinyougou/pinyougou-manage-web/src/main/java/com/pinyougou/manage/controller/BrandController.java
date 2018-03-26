3933d15b752fce7339072f32e0f4a1be36ac4252
liaojianbin <meinitcasthavefun@foxmail.com>
2018-03-26 11:47:06 +0800

    运用商管理后台系统实现品牌新增、修改

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -1,7 +1,9 @@
 package com.pinyougou.manage.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.common.vo.Result;
 import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.pojo.TbItem;
 import com.pinyougou.sellergoods.service.BrandService;
 import com.pinyougou.vo.PageResult;
 import org.springframework.web.bind.annotation.*;
@@ -44,4 +46,47 @@ public class BrandController {
                                @RequestParam(value = "rows", defaultValue =  "10")Integer rows){
         return brandService.findPage(page, rows);
     }
+
+    /**
+     * 保存品牌数据
+     * @param tbBrand 品牌
+     * @return 操作结果
+     */
+    @PostMapping("/add")
+    public Result add(@RequestBody TbBrand tbBrand){
+        try {
+            brandService.add(tbBrand);
+            return Result.ok("新增成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.fail("新增失败");
+    }
+
+    /**
+     * 根据主键查询
+     * @param id 品牌id
+     * @return 品牌
+     */
+    @GetMapping("/findOne")
+    public TbBrand findOne(Long id){
+        return brandService.findOne(id);
+    }
+
+    /**
+     * 更新品牌数据
+     * @param tbBrand 品牌
+     * @return 操作结果
+     */
+    @PostMapping("/update")
+    public Result update(@RequestBody TbBrand tbBrand){
+        try {
+            brandService.update(tbBrand);
+            return Result.ok("修改成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.fail("修改失败");
+    }
+
 }
